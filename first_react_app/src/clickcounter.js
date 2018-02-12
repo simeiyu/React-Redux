c05b1c00657e3a28399d128921818294978153a1
meiyu si <simeiyu@huayun.com>
2018-02-12 11:07:20 +0800

    first_react_app

---
+++ first_react_app/src/clickcounter.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+class ClickCounter extends Component {
+    constructor(props) {
+        super(props)
+        this.onClickButton = this.onClickButton.bind(this)
+        this.state = {
+            count: 0
+        }
+    }
+    onClickButton() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    render() {
+        const countStyle = {margin: 16}
+        return (
+            <div style={countStyle}>
+                <button onClick={this.onClickButton}>Click me!</button>
+                <div>
+                    Click count: {this.state.count}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ClickCounter
